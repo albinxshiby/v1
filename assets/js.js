1681ed8c7f154de2496ae2621d1c061fc7fcd5e6
Albin Shiby <145864271+albinxshiby@users.noreply.github.com>
2024-04-20 12:05:16 +0530

    Add files via upload

--- assets/js.js
+++ assets/js.js
@@ -1,11 +1,4 @@
-setTimeout(function() {
-    document.querySelector('.delayed-text').classList.add('visible');
-  }, 2000);
-  
-  setTimeout(function() {
-    document.querySelector('.delayed-text1').classList.add('visible');
-  }, 2500);
-  
+
   function toggleTheme() {
     var html = document.querySelector('html');
     if (html.getAttribute('data-theme') === 'light') {
