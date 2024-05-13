fab17abb8210076054a153196d4b3f39669a5121
Albin Shiby <145864271+albinxshiby@users.noreply.github.com>
2024-05-13 23:29:14 +0530

    Delete assets directory

--- assets/js.js
+++
@@ -1,9 +0,0 @@
-
-  function toggleTheme() {
-    var html = document.querySelector('html');
-    if (html.getAttribute('data-theme') === 'light') {
-        html.setAttribute('data-theme', 'dark');
-    } else {
-        html.setAttribute('data-theme', 'light');
-    }
-}
